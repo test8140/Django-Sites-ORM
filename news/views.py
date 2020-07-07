a9ad8e59b2dc572826aa0503a07468c8b4b38a65
test8140 <test8140@ukr.net>
2020-07-07 13:51:38 +0300

    Controllers and routes.

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    print(request)
+    return HttpResponse('<h1>Hello Dimon</h1>')
+
+def test(request):
+    return HttpResponse('<h1>Programming django</h1>')
