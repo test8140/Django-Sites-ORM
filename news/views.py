6d61c743cd5f0a963727c30b367c0d6e417a3f3c
test8140 <test8140@ukr.net>
2020-07-09 16:10:42 +0300

    Patterns.

--- news/views.py
+++ news/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from .models import News
 
 def index(request):
-    print(request)
-    return HttpResponse('<h1>Hello Dimon</h1>')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+    }
+    return render(request, template_name='news/index.html', context=context)
 
-def test(request):
-    return HttpResponse('<h1>Programming django</h1>')
