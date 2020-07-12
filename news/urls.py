ef165f0e61c94e606974d47a1ce5318f1dc95187
test8140 <test8140@ukr.net>
2020-07-12 12:58:32 +0300

    Work with forms. Part 1.

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', index, name='home'),
     path('category/<int:category_id>/',  get_category, name='category'),
     path('news/<int:news_id>/', view_views, name='view_views'),
+    path('news/add-news/', add_news, name='add_news'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
-
 from .models import News, Category
+from .forms import NewsForm
 
 def index(request):
     news = News.objects.all()
@@ -24,6 +24,15 @@ def get_category(request, category_id):
 def view_views(request, news_id):
     news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {'news_item': news_item})
+
+
+def add_news(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
+
     
 
 
