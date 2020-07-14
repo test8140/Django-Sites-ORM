94b307b47ef768733ef12678eff9323d9e59be7d
test8140 <test8140@ukr.net>
2020-07-14 14:23:51 +0300

    Class ListView. Part 2.

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', HomeNews.as_view(), name='home'),
-    path('category/<int:category_id>/',  get_category, name='category'),
+    path('category/<int:category_id>/',  NewsByCategory.as_view(
+        #extra_context={'title': 'Какойто тайтл'}
+    ), name='category'),
     path('news/<int:news_id>/', view_views, name='view_views'),
     path('news/add-news/', add_news, name='add_news'),
 ]
