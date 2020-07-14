2a6dda93c3a4cfd6ddd33b314fe85e86c3bbf3a8
test8140 <test8140@ukr.net>
2020-07-14 13:16:35 +0300

    Class  ListView.  Part 1.

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/',  get_category, name='category'),
     path('news/<int:news_id>/', view_views, name='view_views'),
     path('news/add-news/', add_news, name='add_news'),
