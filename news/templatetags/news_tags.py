06a19a98c34c3a4c98a014daf46b0405d64cbcc2
test8140 <test8140@ukr.net>
2020-07-17 18:42:40 +0300

    Django  ORM.   Part  6.

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 
 from news.models import Category
@@ -13,5 +14,6 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories():
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {'categories': categories}
