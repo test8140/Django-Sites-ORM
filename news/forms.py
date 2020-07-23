38268921791f32ccc0720ec94089ac056e8de211
test8140 <test8140@ukr.net>
2020-07-23 14:01:01 +0300

    Authorization.

--- news/forms.py
+++ news/forms.py
@@ -1,11 +1,16 @@
 from django import forms
 from django.core.exceptions import ValidationError
 import re
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 from .models import News
 
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя',  
+               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
 class UserRegisterForm(UserCreationForm):
--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
     #path('test/', test, name='test'),
     path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/',  NewsByCategory.as_view(
