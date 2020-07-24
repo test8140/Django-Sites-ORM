b2e42493e5251848b7aad1c918509260a7e4cb1f
test8140 <test8140@ukr.net>
2020-07-24 16:59:12 +0300

    Caching.

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.db.models import Count, F
+from django.core.cache import cache 
 
 
 from news.models import Category
@@ -14,7 +15,9 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories():
-    # categories = Category.objects.all()
-    categories = Category.objects.annotate(cnt=Count('news', filter=F(
+    categories = cache.get('categories')
+    if not categories:
+        categories = Category.objects.annotate(cnt=Count('news', filter=F(
                 'news__is_published'))).filter(cnt__gt=0)
+        cache.set('categories', categories, 30)
     return {'categories': categories}
--- news/urls.py
+++ news/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from .views import *
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('contact/', contact, name='contact'),
+    #path('', cache_page(60) (HomeNews.as_view()), name='home'),
     path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/',  NewsByCategory.as_view(
         #extra_context={'title': 'Какойто тайтл'}
